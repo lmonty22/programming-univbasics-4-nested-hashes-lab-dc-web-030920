6c9ed6edd51b1943e47d1c67a04732f872f57d20
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-24 21:07:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+    if !programmer_hash[:yukihiro_matsumoto]
      programmer_hash[:yukihiro_matsumoto] = {}
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
